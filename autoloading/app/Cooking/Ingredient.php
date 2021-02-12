380c4abcbcdd13dbe7012ac69f48da36e63ca745
Sam Archie <samuelarchie1@gmail.com>
2021-02-12 12:23:38 +0000

    updates the ingredient class

--- autoloading/app/Cooking/Ingredient.php
+++ autoloading/app/Cooking/Ingredient.php
@@ -5,19 +5,22 @@ namespace App\Cooking;
 class Ingredient
 {
     private $dietryInfo;
+    private $name;
 
-    public function __construct($dietryInfo)
+    public function __construct($name, $dietryInfo)
     {
-        $this->dietryInfo = $dietryInfo;
+        $this->dietryInfo = collect($dietryInfo);
+        $this->name = $name;
     }
 
     public function name()
     {
-        
+        return $this->name;
     }
+
     public function vegan()
     {
-
+        return !$this->dietryInfo->contains("animal produce");
     }
 
 }
